1a0b9ddd9216d7e49067bf50824236ebe2377671
ProgramingChicken <aquacrazz.gp@gmail.com>
2022-07-02 20:48:55 -0700

    fixed json error msg 2

--- middleware/async-handler.js
+++ middleware/async-handler.js
@@ -2,13 +2,16 @@ exports.asyncHandler = (cb) => {
     return async (req, res, next) => {
         try {
 
-            await cb(req, res, next);
+            await cb(req, res, next)
 
         } catch (err) {
             // new book error
             if (err.name === 'SequelizeValidationError') {
                 //didnt find page error
-                res.status(400).json(err.message).end()
+                let newJson = []
+                for (let i = 0; i < err.errors.length; i++) {newJson.push({"message":err.errors[i].message})}
+                console.log(newJson)
+                res.status(400).json(newJson).end()
             } else if (err.name === 'GlobalErrorHandler'){
                 console.log('didnt find server')
                 res.status(500).json(err.message).end()
